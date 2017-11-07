dea02fcc0bd8dc20ef0edfaf592e0f4e0fdcd792
moscowjane <janedesbarats91@gmail.com>
2017-11-07 04:44:45 +0000

    avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,21 +1,46 @@
-var request = require('request', 'secrets');
+  var request = require('request');
+  var secrets = require('./secrets');
+  var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+  console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Access-token': 'f4b0b349fac15e21f0a767d2c2900ebc106ab27a',
-    }
+  function getRepoContributors(repoOwner, repoName, callback) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Access-token': secrets["GITHUB_TOKEN"],
+      }
+    };
+
+    request(options, function(err, res, body) {
+        const results = JSON.parse(body);
+        callback(err, results);
+    });
   }
-  request(options, function(err, res, body) {
-    cb(err, body);
-  })
-}
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+
+  getRepoContributors("jquery", "jquery", function(err, results) {
+
+    console.log("Errors:", err);
+    console.log("Result:", results);
+
+    results.forEach(function(result) {
+      // const filePath1 = `./${result["login"]}.jpg`;
+      const filePath2 = './downloads/' + result["login"] + '.jpg';
+      downloadImageByURL(result["avatar_url"], filePath2);
+      console.log("Avatar_url: " + result["avatar_url"]);
+    });
+  })
+
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+       .on('error', function (err) {
+       })
+       .on('response', function (response) {
+         console.log('File downloaded: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+  }
+
+
